3ed684a9e4ecd3e75c2a69a980be983efe49367d
ShivaSuryaVasan <shivasuryavasan@gmail.com>
2022-08-06 16:12:50 +0530

    Commit

--- resources/views/employee/employee.blade.php
+++ resources/views/employee/employee.blade.php
@@ -6,25 +6,31 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">{{ __('Employee Details') }}</div>
+                <div class="card-header">{{ __('Basic Detail Form') }}</div>
                     <table id="showtable" class="table table-striped table-bordered table-responsive" width="100%">
-
                         <thead>
-                            <tr>
-                                @if(Auth::user()->role == 'superadmin')
-                                    <th></th>
-                                @endif
+                        <tr>
 
-                                <th>Id</th>
-                                <th>Name</th>
-                                <th>Role</th>
-                                <th>Panchayat_name</th>
-                                <th>Email</th>
-                            </tr>
-                            </thead>
-<!-- Footer -->
-                            <tfoot>
+                            @if(Auth::user()->role == 'superadmin')
+                                <th></th>
+                            @endif
+
+                            <th>Id</th>
+                            <th>Name</th>
+                            <th>Role</th>
+                            <th>Panchayat_name</th>
+                            <th>Email</th>
+
+                            @if(Auth::user()->role == 'admin')
+                                <th>Entered_id</th>
+                            @endif
+
+                        </tr>
+                        </thead>
+                        
+                        <tfoot>
                             <tr>
+                                
                                 @if(Auth::user()->role == 'superadmin')
                                     <th></th>
                                 @endif
@@ -34,52 +40,55 @@
                                 <th class="th">Role</th>
                                 <th class="th">Panchayat_name</th>
                                 <th class="th">Email</th>
+
+                                @if(Auth::user()->role == 'admin')
+                                    <th class="th">Entered_id</th>
+                                @endif
+
                             </tr>
-                            </tfoot>
-<!-- End Footer -->
-                            <tbody>
+                        </tfoot>
+                        <tbody>
+                            
+                        @foreach($values as $value)
 
-                            @foreach($values as $value)
+                            @if(Auth::user()->role == 'admin')
 
-                            <tr>
+                                @if($value['Panchayat_name'] == Auth::user()->panchayat_name)
 
-                                @if(Auth::user()->role == 'superadmin')
+                                    @if($value['role'] != Auth::user()->role)
 
-                                    @if($value['role'] != 'superadmin')
-                                        <td>
-                                            <a href={{ "/delete_employee/".$value['id'] }} class="btn btn-primary btn-sm">Delete</a>
-                                        </td>
-                                        <td>{{ $value['id']}}</td>
-                                        <td>{{ $value['name']}}</td>
-                                        <td>{{ $value['role']}}</td>
-                                        <td>{{ $value['panchayat_name']}}</td>
-                                        <td>{{ $value['email']}}</td>
-
-                                    @endif
-
-                                @elseif(Auth::user()->role == 'admin')
-
-                                    @if($value['panchayat_name'] == Auth::user()->panchayat_name)
-
-                                        @if($value['role'] != 'admin')
-
+                                        <tr>
                                             <td>{{ $value['id']}}</td>
                                             <td>{{ $value['name']}}</td>
                                             <td>{{ $value['role']}}</td>
                                             <td>{{ $value['panchayat_name']}}</td>
                                             <td>{{ $value['email']}}</td>
-                                        @endif
-
+                                            <td>{{ $value['Entered_id']}}</td>
                                     @endif
 
                                 @endif
 
-                            </tr>
+                            @else
 
-                            @endforeach
+                                @if($value['role'] != Auth::user()->role)
+                                    
+                                    <tr>
+                                    <td>
+                                        <a href={{ "/delete_employee/".$value['id'] }}  class="btn btn-primary btn-sm">Delete</a>
+                                    </td>
+                                    <td>{{ $value['id']}}</td>
+                                    <td>{{ $value['name']}}</td>
+                                    <td>{{ $value['role']}}</td>
+                                    <td>{{ $value['panchayat_name']}}</td>
+                                    <td>{{ $value['email']}}</td>
+                                @endif
+
+                            @endif
+                            
+                            </tr>
+                        @endforeach
 
                         </tbody>
-
                     </table>
                 <div style="margin-bottom:2em;">
                     <a class="btn btn-primary" style="margin-left:2em;" href="\{{ Auth::user()->role }}" role="button">Back</a>
