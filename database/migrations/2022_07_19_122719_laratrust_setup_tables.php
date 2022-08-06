329e74ffc9222aa547e2e02cce3cf7b36bb83b58
ShivaSuryaVasan <shivasuryavasan@gmail.com>
2022-08-07 00:38:25 +0530

    commit

--- database/migrations/2022_07_19_122719_laratrust_setup_tables.php
+++ database/migrations/2022_07_19_122719_laratrust_setup_tables.php
@@ -16,7 +16,7 @@ class LaratrustSetupTables extends Migration
         // Create table for storing roles
         Schema::create('roles', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name')->unique();
+            $table->string('name')->nullable();
             $table->string('display_name')->nullable();
             $table->string('description')->nullable();
             $table->timestamps();
