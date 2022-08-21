31d69c756b3cb9a093e5d7e86c40ca1e659d1145
ShivaSuryaVasan <shivasuryavasan@gmail.com>
2022-08-22 00:07:35 +0530

    Fix : change size of block name only

--- database/migrations/2022_07_22_140757_create_table7s_table.php
+++ database/migrations/2022_07_22_140757_create_table7s_table.php
@@ -15,19 +15,19 @@ class CreateTable7sTable extends Migration
     {
         Schema::create('table7s', function (Blueprint $table) {
             $table->id();
-            $table->string('Panchayat_name',10);
-            $table->integer('Garbage_process',10);
-            $table->integer('Nengaged_collectors',10)->nullable();
-            $table->integer('Collected_volume',10)->nullable();
-            $table->integer('Bio_waste_volume',10)->nullable();
-            $table->integer('Flexible_waste',10)->nullable();
-            $table->integer('Recycling_waste',10)->nullable();
-            $table->integer('Impossible_recycling',10)->nullable();
-            $table->integer('Medical_waste',10)->nullable();
-            $table->integer('Industrial_waste',10)->nullable();
-            $table->integer('Health_waste',10)->nullable();
-            $table->integer('Electronic_waste',10)->nullable();
-            $table->integer('Contruction_waste',10)->nullable();
+            $table->string('Panchayat_name');
+            $table->integer('Garbage_process');
+            $table->integer('Nengaged_collectors')->nullable();
+            $table->integer('Collected_volume')->nullable();
+            $table->integer('Bio_waste_volume')->nullable();
+            $table->integer('Flexible_waste')->nullable();
+            $table->integer('Recycling_waste')->nullable();
+            $table->integer('Impossible_recycling')->nullable();
+            $table->integer('Medical_waste')->nullable();
+            $table->integer('Industrial_waste')->nullable();
+            $table->integer('Health_waste')->nullable();
+            $table->integer('Electronic_waste')->nullable();
+            $table->integer('Contruction_waste')->nullable();
             $table->integer('Entered_id');
             $table->timestamps();
         });
